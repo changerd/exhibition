452494ec8973a5065a227c92fbf4ef92b1b14efc
Slava Morza <bloodreek@gmail.com>
2024-07-07 10:14:03 +0300

    fix routes

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-    const config = {
-        onHost: false,
-        repoName: 'exhibition'
-    }
 
-    if(config.onHost) {
-        $('a').attr('href', function(i, val) {
-            if(val.includes(config.repoName))
-                return val;
-            else
-                return config.repoName + val;
-        });
-    }
 })
